d3d79fe2c6f768f78fe20f8ef37c47ee5e7befe4
Sergio <45878759+scullen99@users.noreply.github.com>
2020-06-08 14:17:22 +0200

    Add files via upload

---
+++ Queries/Queries.sql
@@ -0,0 +1,42 @@
+-- Queries BBDD SmallCompany --
+-- Sergio Esteban Tarrero --
+
+/*
+1 - Se necesita prejubilar a dos personas: estimar las menos costosas teniendo en cuenta la edad,
+a los 65 años se jubilan, y que por cada año de antigüedad se debe pagar un 15% del sueldo
+bruto anual en concepto de indemnización. */
+
+
+/*
+2 - Se va a arrancar un proyecto nuevo con un presupuesto de 150.000 euros para dos años y se 
+necesitan 4 personas, averiguar si se podrían cubrir los puestos con las 4 personas disponibles
+que tengan el sueldo más bajo. */
+
+
+/*
+3 - A final de año el IPC sube un 1,2%, aplicarlo al sueldo de todos los empleados. */
+
+
+/*
+4 - Subir el sueldo 2000 € al año a los empleados con más de 15 años de antigüedad y 
+sueldo inferior a 25.000€. */
+
+
+/*
+5 - Hacer todas las actualizaciones necesarias para cerrar un proyecto sin despedir a nadie, y 
+devolviendo el presupuesto, si quedara, al proyecto de su mismo departamento que tenga
+el menor presupuesto. */
+
+
+/*
+6 - Obtener la lista de proyectos con menos de 4 empleados. */
+
+
+/*
+7 - Se quieren buscar cinco puestos para los empleados de un proyecto, buscar las salas en las que
+sería posible ubicarles, todos en la misma sala. */
+
+
+/*
+8 - Obtener para cada proyecto el empleado de menos antigüedad. */
+
